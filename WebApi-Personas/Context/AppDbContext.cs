b2e8b66e9e0aad4df64c7b6b9bf1cc7df1029aba
unknown <pablolec@hotmail.com>
2024-10-27 15:36:47 -0300

    primera subida

---
+++ WebApi-Personas/Context/AppDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebApi_Personas.Models;
+
+namespace WebApi_Personas.Context
+{
+    public class AppDbContext : DbContext //Herencia de clase de entityFramework (ORM)
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+
+
+    public required DbSet<Person> Persons { get; set; }
+
+
+    }
+}
